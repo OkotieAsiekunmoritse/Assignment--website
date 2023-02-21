25a2fa18505d275ee8fbbfac29ab268635208376
Okotie Asiekunmoritse <108562214+OkotieAsiekunmoritse@users.noreply.github.com>
2023-02-21 01:18:54 +0100

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,8 +11,8 @@ class About extends Component {
                     <span className={classes.head}>ABOUT ME</span>
                     <h2 className={classes.heading}>Who Am I?</h2>
                     <div className={classes.About}>
-                        <p> My name is <b>SACHIN MITTAL</b> and I am currently working as Software Developer Engineer at <a target="_blank" href="https://www.springworks.in/"><b>Springworks</b></a>. I completed my degree in Bachelor of Technology in Computer Science and Engineering from Sant Longowal Institute of Engineering and Technology. I am much interested in developing new things which excite me a lot. :)  </p>
-                        <p className={classes.br}>I love exploring new technologies and being a practitioner, I like to stay on top of latest trends. I try to leave every line of code I write more readable, accessible, and modular.
+                        <p> My name is <b>OKOTIE ASIEKUNMORITSE</b> I am currently a cloud Engineering student in Altschool, I love coding, techinical writing and reading. </p>
+                        <p className={classes.br}>I love exploring new technologies, I like to stay on top of latest trends.
                         </p>
                     </div>
                 </ScrollAnimation>
