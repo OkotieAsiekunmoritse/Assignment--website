1e8b434bca8a4a4ec4cd9aa9ad006c43d999f00d
Okotie Asiekunmoritse <108562214+OkotieAsiekunmoritse@users.noreply.github.com>
2023-02-21 01:49:33 +0100

    Update Sidebar.js

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Sidebar.css';
 import { HashLink as Link } from 'react-router-hash-link';
 import { FiExternalLink } from 'react-icons/fi';
-import logo from '../images/sam.jpg'
+import logo from '../images/mo.jpg'
 
 class Sidebar extends Component {
     render() {
@@ -12,10 +12,11 @@ class Sidebar extends Component {
                 <div style={{ color: 'black', fontWeight: 'bold' }} className="tagtop"> # programmer_life </div>
                 <div style={{ color: 'black', fontWeight: 'bold' }} className=""> # hello_world </div>
                 <div style={{ color: 'black', fontWeight: 'bold' }} className=""> # coding </div>
-                <h1><Link smooth to="/#start" className="h1_links">Sachin Mittal</Link></h1>
+                <h1><Link smooth to="/#start" className="h1_links">Okotie Asiekunmoritse</Link></h1>
 
-                <img src={logo} />
-                <p style={{ color: 'black', fontWeight: 'bold' }} className="gmail"><a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJftvBffjPmxWxvfHWRHWnhTbpQgPxBxvkcnPpFpFqbhrXMCHXSJTsCXCVLsQzgRcVLpnPL" rel="opener noreferrer" target="_blank" className="fa fa-envelope"></a> mittalsam98@gmail.com </p>
+                <img src={logo} />  
+                <p style={{ color: 'black', fontWeight: 'bold' }} className="gmail"><a href="mailto:sillomo94@gmail.com" rel="opener noreferrer" target="_blank" className="fa fa-envelope"></a> sillomo94@gmail.com </p>
+               
 
                 <ul className="sidebar-nav">
                     <li className="sidebar-nav-items"><Link smooth to="/#about" className="links">About</Link></li>
@@ -26,12 +27,11 @@ class Sidebar extends Component {
 
                 <div className="flip-card-back">
                     <ul className="sidebar-nav">
-                        <li className="sidebar-nav-icons"> <a href="https://github.com/mittalsam98" rel="opener noreferrer" target="_blank" className="fa fas fa-github fa-lg"></a></li>
-                        <li className="sidebar-nav-icons"><a href="https://www.linkedin.com/in/sachin-mittal-476174158?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BoRhF2EUsQJ%2BygJpLEZb%2FFA%3D%3D" rel="opener noreferrer" target="_blank" className="fa fas fa-linkedin fa-lg"></a></li>
-                        <li className="sidebar-nav-icons"> <a href="https://twitter.com/Sachin_Mittal98" rel="opener noreferrer" target="_blank" className="fa fas fa-twitter fa-lg"></a></li>
-                        <li className="sidebar-nav-icons"> <a href="https://www.instagram.com/decent_sachin.mittal/" rel="opener noreferrer" target="_blank" className="fa fas fa-instagram fa-lg"></a></li>
-                        <li className="sidebar-nav-icons"> <a href="mailto:mittalsam98@gmail.com" rel="opener noreferrer" target="_blank" className="fa fas fa-envelope fa-lg"></a></li>
-                        <li className="sidebar-nav-icons"> <a href="https://www.facebook.com/profile.php?id=100011989067867" rel="opener noreferrer" target="_blank" className="fa fas fa-facebook fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="https://github.com/OkotieAsiekunmoritse" rel="opener noreferrer" target="_blank" className="fa fas fa-github fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"><a href="https://www.linkedin.com/in/okotie-asiekunmoritse-23b134145/" rel="opener noreferrer" target="_blank" className="fa fas fa-linkedin fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="https://www.instagram.com/mor_itse/" rel="opener noreferrer" target="_blank" className="fa fas fa-instagram fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="mailto:sillomo94@gmail.com" rel="opener noreferrer" target="_blank" className="fa fas fa-envelope fa-lg"></a></li>
+                        
                     </ul>
                 </div>
 
